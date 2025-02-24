156e4b9c91d1c6decc1fbaec69fff40fca1308e2
Naman Shukla <namanshukla256@gmail.com>
2025-02-24 13:51:42 +0530

    24

--- GFG-Top 50 JavaScript Coding questions/Basic JavaScript Coding questions and answers/15.Sum of an array.js
+++ GFG-Top 50 JavaScript Coding questions/Basic JavaScript Coding questions and answers/15.Sum of an array.js
@@ -1,1 +1,10 @@
-function sumArray
+function sumArray(arr){
+    let sum = 0;    
+    for (let i = 0; i< arr.length; i++) {
+        sum = sum + arr[i];
+    }
+
+    return sum;
+}
+
+console.log(sumArray([2, 4, 6, 8])); //20
